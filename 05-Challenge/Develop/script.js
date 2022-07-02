56f40b94abb06b28aa12a5bd75436365fc35a3c4
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-07-02 12:28:28 -0500

    homework set up portion

---
+++ 05-Challenge/Develop/script.js
@@ -0,0 +1,23 @@
+// FUNCTIONALITY FOR SAVING NOTES
+// Listen for a button click on the save button
+// grab the value from the asssociated text area
+// save value to localStorage
+
+// GETTING PREVIOUS SAVED VALUES, AND DISPLAYING ON THE PAGE
+// get value (saved note) from local storage
+// give the text area the value that we got from localStorage
+
+//FUNCTIONALITY FOR CHANGING BACKGROUND COLOR BASED ON THE TIME OF DAY
+// Compare the current time of day with teh time associated with each block - use moment.js
+// set up variable equal to the current time of day with the time associated with each block - use moment.js
+// loop over the blocks
+// compare the blocks time to the current time variable that we set up
+// if blocks time < current time variable - change background color to gray
+
+// else if the blocks time is equal to the current hour - change background color to red
+// else - set it to green
+
+//DISPLAY THE CURRENT DAY ON THE PAGE
+// USE MOMENT TO GRAB THIS DAY/DATE
+// FORMAT IT CORRECTLY
+// PUT IT ON THE PAGE USING THE PARAGRAPH TAG
