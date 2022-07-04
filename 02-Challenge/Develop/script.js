65d808abf3eca48bfbcda114bfc846992c58fbe4
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-07-04 14:41:42 -0500

    Time blocks showing up, being logged correctly

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,11 +1,11 @@
 var $currentDay = $("#currentDay");
 var $timeBlocks = $(".time-block");
 var $scheduleArea = $(".schedule");
+var saveButtonEl = $('#saveBtn')
 var currentDate = moment().format("dddd, MMM Do [at] hh:mm a")
-var currentHour = moment().format("H");
 var $thisBlock = $(this).parent();
 var userInput = $("textarea");
-var toDoItems = [];
+var todos = [];
 
 
 //DISPLAY THE CURRENT DAY ON THE PAGE
@@ -13,28 +13,8 @@ var toDoItems = [];
 // USE MOMENT TO GRAB THIS DAY/DATE
 // FORMAT IT CORRECTLY
 // PUT IT ON THE PAGE USING THE PARAGRAPH TAG
-
 var today = moment();
     $("#currentDay").text(today.format("dddd MMM Do, YYYY [at] hh:mm a"));
-$(document).ready(function() {
-    $currentDay.text(currentDate);
-
-    $timeBlocks.each(function () {
-        var $thisBlock = $(this);
-        var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
-
-        if (thisBlockHr == currentHour) {
-        $thisBlock.addClass("present").removeClass("past future");
-        }
-        if (thisBlockHr < currentHour) {
-        $thisBlock.addClass("past").removeClass("present future");
-        }
-        if (thisBlockHr > currentHour) {
-        $thisBlock.addClass("future").removeClass("past present");
-        }
-
-})
-})
 
 //FUNCTIONALITY FOR CHANGING BACKGROUND COLOR BASED ON THE TIME OF DAY
 // Compare the current time of day with teh time associated with each block - use moment.js
@@ -46,11 +26,78 @@ $(document).ready(function() {
 // else if the blocks time is equal to the current hour - change background color to red
 // else - set it to green
 
+$(document).ready(function() {
+    $currentDay.text(currentDate);
+    var currentHour = moment().hour();
+$timeBlocks.each(function () {
+    var $thisBlock = $(this);
+    var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+
+    if (thisBlockHr == currentHour) {
+    $thisBlock.addClass("present").removeClass("past future");
+    }
+    if (thisBlockHr < currentHour) {
+    $thisBlock.addClass("past").removeClass("present future");
+    }
+    if (thisBlockHr > currentHour) {
+        $thisBlock.addClass("future").removeClass("past present");
+    }
+    console.log(currentHour)
+});
+
 // FUNCTIONALITY FOR SAVING NOTES
 // Listen for a button click on the save button
 // grab the value from the asssociated text area
 // save value to localStorage
 
-// GETTING PREVIOUS SAVED VALUES, AND DISPLAYING ON THE PAGE
-// get value (saved note) from local storage
-// give the text area the value that we got from localStorage
+
+
+$(".saveBtn").on('click', function () {
+    var Text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr('id');
+    // alert("Your event has been saved");
+    localStorage.setItem(time, Text)
+    console.log(time, Text)
+    // localStorage.setItem("userInput", JSON.stringify(userInput));
+    // userInput.append;
+})
+
+function renderTodos(){
+    toDoItems = localStorage.getItem("todos");
+    toDoItems = JSON.parse(toDoItems);
+    for (var i = 0; i < toDoItems.length; i++) {
+        var itemHour = toDoItems[i].hour;
+        var itemText = toDoItems[i].text;
+        $("data-hour=" + itemHour + "]").children("textarea").val(itemText);
+    }
+}
+function init() {
+    var storedTodos = JSON.parse(localStorage.getItem ("todos"));
+
+    if (storedTodos !== null) {
+        todos = storedTodos;
+    }
+    renderTodos();
+}
+
+function storeTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    storeTodos();
+    renderTodos();
+}
+
+// // GETTING PREVIOUS SAVED VALUES, AND DISPLAYING ON THE PAGE
+// // get value (saved note) from local storage
+// // give the text area the value that we got from localStorage}
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+})
